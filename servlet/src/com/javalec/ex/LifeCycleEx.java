6dfd845704b9067e01ebd47ed6b5ad68c6a495b7
ineggapps <inegg.apps@gmail.com>
2020-02-08 20:40:53 +0900

    라이프사이클

---
+++ servlet/src/com/javalec/ex/LifeCycleEx.java
@@ -0,0 +1,57 @@
+package com.javalec.ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LifeCycleEx
+ */
+@WebServlet("/LifeCycleEx")
+public class LifeCycleEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LifeCycleEx() {
+        super();
+        System.out.println("서블릿 생성 중... 생성자 호출됨");
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void init() throws ServletException {
+    	// TODO Auto-generated method stub
+    	System.out.println("init() 메서드가 호출됨");
+    }
+    
+    
+    @Override
+    public void destroy() {
+    	// TODO Auto-generated method stub
+    	System.out.println("destroy() 메서드가 호출됨");
+    }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet 메서드랍니다");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost  메서드랍니다");
+		doGet(request, response);
+	}
+
+}

0f5c339970b5272be49b7dfe334d22b463207363
ineggapps <inegg.apps@gmail.com>
2020-03-06 22:12:57 +0900

    doGet방식과 doPost방식 사용 예제

---
+++ servlet_lec05_01_servlet/src/com/javalec/ex/PostMethod.java
@@ -0,0 +1,53 @@
+package com.javalec.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PostMethod
+ */
+@WebServlet("/PostMethod")
+public class PostMethod extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PostMethod() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=EUC-KR");
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h1>Hello World in Post Method</h1>");
+		writer.println("</body>");
+		writer.println("</html>");
+		writer.close();
+		System.out.println("doPost() 메서드 실행되었습니다.");
+	}
+
+}
